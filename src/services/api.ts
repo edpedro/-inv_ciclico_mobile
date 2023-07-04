398516194f27714b4c3d0739b2e908f6e59208ff
Eduardo <edp2013.ep@gmail.com>
2023-07-03 23:00:57 -0300

    url

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   //baseURL: "http://192.168.0.108:3000",
-  baseURL: "https://invciclicobackend-production.up.railway.app",
+  baseURL: "https://invciclico.adaptable.app",
 });
 
 export default api;
